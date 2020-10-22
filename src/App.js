a9098742aa3ebb982bdd9d03f8224a7d1634a9cb
Shashank Biplav <Biplavshashank7@gmail.com>
2020-10-22 14:37:33 +0530

    Firebase collection setup in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
 
+const firebase = require('firebase');
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedDocIndex: null,
+            selectedDoc: null,
+            documents: null
+        };
+    }
+
     render() {
         return (<div>Hello World</div>);
     }
+
+    componentDidMount = () => {
+        firebase
+            .firestore()
+            .collection('docs')
+            .onSnapshot(serverUpdate => {
+                const notes = serverUpdate.docs.map(_doc => {
+                    const data = _doc.data();
+                    data['id'] = _doc.id;
+                    return data;
+                });
+                console.log(notes);
+                this.setState({documents: notes});
+            });
+    };
+
 }
 
 export default App;
